ece7c443ae4521e153c2ed48f81b2980c156eba7
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-04-24 09:58:51 +0530

    κιcнusᴇʀ✨

---
+++ Kichu/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2022 Kichu.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Kichu
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
